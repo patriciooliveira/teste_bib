f05724b31e1f460a82be84bc708fd992418dea31
ricavalcanti <ricjose2005@hotmail.com>
2016-11-04 11:29:19 -0200

    Add files via upload

---
+++ teste_Basico04_11.ino
@@ -0,0 +1,16 @@
+#include <Basico.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+Basico esp("hidro", "hidrometro");
+esp.conecta("Robotica-IMD", "roboticawifi");
+Serial.begin(115200);
+Serial.print(esp.getIp());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(".");
+}
