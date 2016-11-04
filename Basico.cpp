84f451a22b450a03e3ad8d0b7d7c868cefdc0834
ricavalcanti <ricjose2005@hotmail.com>
2016-11-04 11:28:47 -0200

    Add files via upload

--- Basico.cpp
+++ Basico.cpp
@@ -8,49 +8,35 @@
 //    this->t_com = "nenhuma comunicacao";
 
 //}
-Basico::Basico(char t[], char d[])
+Basico::Basico(char *t, char *d)
 {
     this->tag = t;
     this->desc = d;
 }//construtor
 
-void Basico::setTag(char t[])
+void Basico::setTag(char *t)
 {
     this->tag = t;
 }
 
-void Basico::conecta(char rede[], char senha[])
+IPAddress Basico::getIp()
+{
+    return this->ip_local;
+}
+
+void Basico::conecta(char *rede, char *senha)
 {
     WiFi.begin(rede, senha);
     while(WiFi.status() != WL_CONNECTED)
     {
         delay(500);
     }
-    this->ip = WiFi.localIP();
-    
+    this->ip_local = WiFi.localIP();
+
     //PARA TESTE
-       
-    
+
+
 }
 
-void Basico::post(int valor)
-{
-      HTTPClient http;//cria o objeto http
-      http.begin("http://" + "aqui vai o ip do nó central" + "/yii/web/index.php?r=sensor/create/");
-      http.addHeader("Content-Type", "application/json; charset=UTF-8");
-      int httpCode = http.POST("{\"tag\":\"ESP-4\",\"valor\":" +
-                               String(valor) +
-                               ",\"data_hora\":\"2016-" +
-                               String(random(1,13)) +
-                               "-02 11:11:11\"}");//Retorna o código http, caso não conecte irá retornar -1
-
-      //      String payload = http.getString();
-
-      //      Serial.print(httpCode);
-
-      //      Serial.println(" " + payload);
-      http.end();
-
-}//faz isso de forma genérica, utilizando a tag do objeto aqui
 
 
--- Basico.h
+++ Basico.h
@@ -4,32 +4,33 @@
 
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <WiFi.h>
+//#include <WiFi.h>
 
 #include <Arduino.h>        ///usar as funçoes do arduino
-#include <ArduinoJson.h>    ///usar a bib Json
+//#include <ArduinoJson.h>    ///usar a bib Json
 #include <EEPROM.h>         /// ler/escrer na EEPROM
 
-#include <cstring>
 
 
 class Basico
 {
 private:
-    char tag[50], desc[100]; //t_com, param, consumo;
+    char *tag;
+    char *desc; //t_com, param, consumo;
     IPAddress ip_local;
 
 public:
-    Basico(String, String);  //construtor
-    void setTag(char []);// permitir alteração da tag
-    void conecta(char [], char []);
+    Basico(char *, char *);  //construtor
+    void setTag(char *);// permitir alteração da tag
+    void conecta(char *, char *);
+    IPAddress getIp();
 
 //    void post(int);
 //    void liga(); //alimenta o atuador
 //    void desliga(); // interrrompe o atuador
 //    void getValorAtuador();
 //    bool serValorAtuador(int novoValor);
-   
+
 };
 
 #endif
